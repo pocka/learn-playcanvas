77e0e5357cfdddd4a4a48d3e2eb6c7a706966537
Shota Fuji <pockawoooh@gmail.com>
2020-04-14 03:28:11 +0000

    fix: Change shadow type
    To fix weird rendering result.

--- src/entities/light.ts
+++ src/entities/light.ts
@@ -5,7 +5,7 @@ export const light = new pc.Entity('light')
 // TODO: Fix the weird light problem
 light.addComponent('light', {
   castShadows: true,
-  shadowType: pc.SHADOW_PCF5,
+  shadowType: pc.SHADOW_VSM32,
   vsmBlurMode: pc.BLUR_GAUSSIAN
 } as pc.LightComponent)
 
